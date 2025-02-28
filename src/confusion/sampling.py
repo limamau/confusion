5bdf201ca8ee22c1d07f0e6182089e1e178733f7
Mauricio Lima <mauriciodemouralima@gmail.com>
2025-02-28 17:11:20 +0100

    add sampler abstraction class

--- src/confusion/sampling.py
+++ src/confusion/sampling.py
@@ -1,7 +1,10 @@
+from abc import ABC, abstractmethod
+from functools import partial
 from typing import Tuple
 
 import diffrax as dfx
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 import jax.random as jr
 from diffrax import AbstractSolver
@@ -9,96 +12,110 @@ from jaxtyping import Array, Key
 
 from .diffusion import AbstractDiffusionModel
 
-# TODO: code this
-# class AbstractSampler(ABC):
-#     @abstractmethod
-#     @staticmethod
-#     def single_sample_fn(
-#         model: AbstractDiffusionModel,
-#         data_shape: Tuple[int, ...],
-#         dt0: float,
-#         t1: float,
-#         conds: Array | None,
-#         key: Key,
-#     ) -> Array:
-#         pass
 
-#     def sample(
-#         self,
-#         model: AbstractDiffusionModel,
-#         data_shape: Tuple[int, ...],
-#         dt0: float,
-#         t1: float,
-#         conds: Array | None,
-#         key: Key,
-#         norm_mean: Array,
-#         norm_std: Array,
-#         sample_size: int,
-#     ) -> Array:
-#         sample_key = jr.split(key, sample_size)
-#         sample_fn = partial(
-#             self.single_sample_fn,
-#             model,
-#             norm_mean.shape,
-#             dt0,
-#             t1,
-#         )
-#         gen_samples = jax.vmap(sample_fn)(conds, sample_key)
-#         return norm_mean + norm_std * gen_samples
+class AbstractSampler(ABC):
+    @abstractmethod
+    def single_sample_fn(
+        self,
+        model: AbstractDiffusionModel,
+        data_shape: Tuple[int, ...],
+        conds: Array | None,
+        key: Key,
+    ) -> Array:
+        pass
+
+    # TODO: add possibility to use modifiers and use explicitly the score instead of getting it from the model
+    def sample(
+        self,
+        model: AbstractDiffusionModel,
+        data_shape: Tuple[int, ...],
+        conds: Array | None,
+        key: Key,
+        norm_mean: Array,
+        norm_std: Array,
+        sample_size: int,
+    ) -> Array:
+        sample_key = jr.split(key, sample_size)
+        sample_fn = partial(
+            self.single_sample_fn,
+            model,
+            data_shape,
+        )
+        gen_samples = jax.vmap(sample_fn)(conds, sample_key)
+        return norm_mean + norm_std * gen_samples
 
 
-@eqx.filter_jit
-def single_ode_sample_fn(
-    model: AbstractDiffusionModel,
-    data_shape: Tuple[int, ...],
-    dt0: float,
-    t1: float,
-    conds: Array | None,
-    key: Key,
-    solver: AbstractSolver = dfx.Tsit5(),
-) -> Array:
-    def fn(t, x, args):
-        f = model.drift(x, t)
-        g2 = jnp.square(model.diffusion(t))
-        s = model.score(x, t, conds)
-        return f - 0.5 * g2 * s
+class ODESampler(AbstractSampler):
+    dt0: float
+    t1: float
+    solver: AbstractSolver
 
-    term = dfx.ODETerm(fn)
-    t0 = 0
-    x1 = jr.normal(key, data_shape)
-    # solve from t1 to t0
-    sol = dfx.diffeqsolve(term, solver, t1, t0, -dt0, x1)
+    def __init__(self, dt0: float, t1: float, solver: AbstractSolver = dfx.Tsit5()):
+        self.dt0 = dt0
+        self.t1 = t1
+        self.solver = solver
 
-    assert sol.ys is not None
-    return sol.ys[0]
+    @eqx.filter_jit
+    def single_sample_fn(
+        self,
+        model: AbstractDiffusionModel,
+        data_shape: Tuple[int, ...],
+        conds: Array | None,
+        key: Key,
+    ) -> Array:
+        def fn(t, x, args):
+            f = model.drift(x, t)
+            g2 = jnp.square(model.diffusion(t))
+            s = model.score(x, t, conds)
+            return f - 0.5 * g2 * s
+
+        term = dfx.ODETerm(fn)
+        t0 = 0
+        x1 = jr.normal(key, data_shape)
+        # solve from t1 to t0
+        sol = dfx.diffeqsolve(term, self.solver, self.t1, t0, -self.dt0, x1)
+
+        assert sol.ys is not None
+        return sol.ys[0]
 
 
-@eqx.filter_jit
-def single_sde_sample_fn(
-    model: AbstractDiffusionModel,
-    data_shape: Tuple[int, ...],
-    dt0: float,
-    t1: float,
-    conds: Array | None,
-    key: Key,
-    solver: AbstractSolver = dfx.Euler(),
-) -> Array:
-    def back_drift(t, x, args):
-        f = model.drift(x, t)
-        g2 = jnp.square(model.diffusion(t))
-        s = model.score(x, t, conds)
-        return f - g2 * s
+class SDESampler(AbstractSampler):
+    dt0: float
+    t1: float
+    solver: AbstractSolver
 
-    def back_diffusion(t, x, args):
-        return model.diffusion(t)
+    def __init__(self, dt0: float, t1: float, solver: AbstractSolver = dfx.Euler()):
+        self.dt0 = dt0
+        self.t1 = t1
+        self.solver = solver
 
-    keys = jr.split(key, 2)
-    t0 = 0
-    bm = dfx.VirtualBrownianTree(t0, t1, tol=dt0, shape=(), key=keys[0])
-    terms = dfx.MultiTerm(dfx.ODETerm(back_drift), dfx.ControlTerm(back_diffusion, bm))
-    x1 = jr.normal(keys[1], data_shape)
-    # solve from t1 to t0
-    sol = dfx.diffeqsolve(terms, solver, t1, t0, -dt0, x1)
+    @eqx.filter_jit
+    def single_sample_fn(
+        self,
+        model: AbstractDiffusionModel,
+        data_shape: Tuple[int, ...],
+        conds: Array | None,
+        key: Key,
+        solver: AbstractSolver = dfx.Euler(),
+    ) -> Array:
+        def back_drift(t, x, args):
+            f = model.drift(x, t)
+            g2 = jnp.square(model.diffusion(t))
+            s = model.score(x, t, conds)
+            return f - g2 * s
 
-    assert sol.ys is not None
-    return sol.ys[0]
+        def back_diffusion(t, x, args):
+            return model.diffusion(t)
+
+        keys = jr.split(key, 2)
+        t0 = 0
+        bm = dfx.VirtualBrownianTree(t0, self.t1, tol=self.dt0, shape=(), key=keys[0])
+        terms = dfx.MultiTerm(
+            dfx.ODETerm(back_drift), dfx.ControlTerm(back_diffusion, bm)
+        )
+        x1 = jr.normal(keys[1], data_shape)
+        # solve from t1 to t0
+        sol = dfx.diffeqsolve(terms, solver, self.t1, t0, -self.dt0, x1)
+
+        assert sol.ys is not None
+        return sol.ys[0]
